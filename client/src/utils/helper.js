1eec7731aa20ba3549667e58298ea931ae0c26bc
manish <manishkrsah88@gmail.com>
2024-08-25 16:49:28 +0545

    add helper file

---
+++ client/src/utils/helper.js
@@ -0,0 +1,15 @@
+export const validateEmail=(email)=>{
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; 
+  return re.test(String(email).toLowerCase());
+}
+
+
+export const getInitials=(name)=>{
+  const words = name.split(' ');
+  let initials = '';
+
+  for (let i = 0; i < words.length; i++) {
+    initials += words[i][0];
+  }
+  return initials.toUpperCase();
+}
