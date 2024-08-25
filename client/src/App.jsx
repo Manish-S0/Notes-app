42bc8a91c50d48890c5dcc274a8d5881a7ad8af7
manish <manishkrsah88@gmail.com>
2024-08-25 16:44:50 +0545

    create react app

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+// import { useState } from 'react'
+import Home from './pages/Home/Home'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  
+  return (
+    <>
+      <BrowserRouter>
+
+        <Routes> 
+
+          <Route path='/' element={<Home />}></Route>
+
+          <Route path='/login' element={<Login/>}></Route>
+
+          <Route path='/signup' element={<SignUp/>}></Route>
+
+        </Routes>
+
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
