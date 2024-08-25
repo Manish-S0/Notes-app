3ace18251f1b8b7642b1836aa7bc9ad31f5a4eca
manish <manishkrsah88@gmail.com>
2024-08-25 16:50:23 +0545

    add searchbox

---
+++ client/src/components/SearchBox/Search.jsx
@@ -0,0 +1,22 @@
+
+import {FaMagnifyingGlass} from 'react-icons/fa6'
+import {IoMdClose} from 'react-icons/io'
+const Search = ({value,onChange,handleSearch,onClearSearch}) => {
+  return (
+    <div className='w-80 flex items-center px-4 bg-slate-100 rounded-md'>
+      <input type="text" 
+      value={value} 
+      placeholder="Search Notes"
+      onChange={onChange} className=" w-full text-xs bg-transparent py-[10px] outline-none" />
+
+
+      {value && <IoMdClose size={20} onClick={onClearSearch} className='text-slate-400 cursor-pointer hover:text-black mr-3' />}
+
+      <FaMagnifyingGlass size={20} className='text-slate-400 cursor-pointer hover:text-black' />
+
+      
+    </div>
+  )
+}
+
+export default Search
